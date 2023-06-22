b0d1dada9be84b5d202125228b8d46344da30972
Niranjan-KS <131178323+niranjan-ks-pro@users.noreply.github.com>
2023-06-22 17:54:11 +0530

    7ifStatement.dart

---
+++ 7ifStatement.dart
@@ -0,0 +1,20 @@
+//void main(List<String> args) {
+//var num = -10;
+//if (num > 0) {
+//print("positive number");
+//} //else {
+//print("negative number");
+//}
+//}
+
+void main(List<String> args) {
+  var num = "a";
+
+  if (num == "a") {
+    print("apple");
+  } else if (num == "b") {
+    print("bat");
+  } else if (num == "c") {
+    print("cat");
+  }
+}
